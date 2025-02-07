07bf9810ee3ebe1b2764b150f2f4d200335cf877
Johana Moras <164534009+Johana-Moras-0458@users.noreply.github.com>
2025-02-07 16:47:41 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+//funcion que no tiene valor
+//funcion saludar
+
+void saludar(){
+  print('hola mundo');
+}
+
+void escribenombre(){
+  print('johana moras');
+}
+void main(){
+  //johana moras mat:22308051280458
+  //llamar a la funcion saludar
+  print('johana moras mat:22308051280458');
+  print('llamando a la funcion saludar');
+  saludar();
+  print('llamando a la funcion escribenombre');
+  escribenombre();
+}
