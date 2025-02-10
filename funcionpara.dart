e2e4de9d4ae16c50277e0dcbbee5e7eb70a5722a
Johana Moras <164534009+Johana-Moras-0458@users.noreply.github.com>
2025-02-10 18:50:33 +0000

    funcion parametro

---
+++ funcionpara.dart
@@ -0,0 +1,22 @@
+
+// parametro 1:  saludar a una persona con su nombre y edad
+void saludar(String nombre, int edad) {
+  print("Hola, $nombre! Tienes $edad años.");
+}
+
+// parametro 2: sumar dos números enteros
+int sumar(int a, int b) {
+  return a + b;
+}
+
+void main() {
+print('johana moras martinez 223051280458');
+  // llama a la primer parametro
+   print('llamar parametro nombre y edad');
+  saludar("Ale", 18);
+
+  // Llamada a la segundo parametro
+  print('llamar parametro sumar');
+  int resultado = sumar(5, 3);
+  print("El resultado de la suma es: $resultado");
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -3,3 +3,6 @@ ejemplo de salida
 
 ejemplo de salida funcion normal + funcion flecha
 ![alt text](imagen-1.png)
+
+ejemplo de salida de funcion parametros
+![alt text](imagen-2.png)
